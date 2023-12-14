81395914bac711b9e02d76c73d7c5c0731665ffe
Zachary Dj <zachary@zacharys-mbp.home>
2023-12-14 16:55:37 -0500

    completed menu interface

--- main_game.py
+++ main_game.py
@@ -1,42 +1,64 @@
 import curses
-def main(stdscr):
-    menu = ["Start Game","Exit"]
-    
-    def print_menu(stdscr, selected):
-        '''Prints home_screen'''
-        stdscr.clear()
-        height, width = stdscr.getmaaxy()
-        for idx, row in enumerate(menu):
-            x = width // 2 - len(row) // 2
-            y = height // 2 - len(row) // 2
-            if idx == selected:
-                stdscr.attron(curses.A_REVERSE)
-                stdscr.addstr(y, x , row)
-                stdscr.attroff(curses.A_REVERSE)
-            else:
-                stdscr.attroff(curses.A_REVERSE)
-        stdscr.noutrefresh()
-                
-    def typing_game(stdscr):
-        '''Plays game'''
-        pass
+from curses import wrapper
 
-    def home_screen():
-        '''Lets user choose option in home screen'''
-        row = 0
+
+def print_menu(stdscr, selected):
+    menu = ["Start Game","Tell me a Dad Joke!","Exit"]
+    stdscr.clear()
+    height, width = stdscr.getmaxyx()
+    for idx, row in enumerate(menu):
+        x = width // 2 - len(row) // 2
+        y = height // 2 - len(row) // 2
+        if idx == selected:
+            stdscr.attron(curses.A_REVERSE)
+            stdscr.addstr(y, x , row)
+            stdscr.attroff(curses.A_REVERSE)
+        else:
+            stdscr.addstr(y, x, row)
+        stdscr.noutrefresh()
+        
+        
+def make_me_laugh(stdscr):
+    pass
+                
+                
+def typing_game(stdscr):
+    pass
+    
+    
+def update_home_screen():
+    pass
+        
+        
+def main(stdscr):
+    
+    def home_screen(stdscr):
+        height, width = stdscr.getmaxyx()
+        
+        curses.curs_set(0)
+        current_row = 0
+        print_menu(stdscr, current_row)
+        
+        curses.doupdate()
+        
         while True:
             key = stdscr.getch()
-            if key == curses.KEY_UP and row > 0:
-                row -= 1
-            elif key == curses.KEY_DOWN and row < 1:
-                row += 1
+            if key == curses.KEY_UP and current_row > 0:
+                current_row -= 1
+            elif key == curses.KEY_DOWN and current_row < 2:
+                current_row += 1
             elif key == curses.KEY_ENTER or key in [10,13]:
-                if row == 0:
+                if current_row == 0:
                     typing_game()
-                elif row == 1:
+                if current_row == 1:
+                    make_me_laugh(stdscr)
+                if current_row == 2:
                     break
-        print_menu(stdscr, row)
+        print_menu(stdscr, current_row)
 
                     
     
-    home_screen()
+    home_screen(stdscr)
+
+    
+wrapper(main)
