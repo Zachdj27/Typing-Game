7cd257ee5b80d6199f94ccdcf8318cded96fd650
Zachary Dj <zachary@zacharys-mbp.home>
2023-12-14 17:33:27 -0500

    fixed home screen bugs

--- main_game.py
+++ main_game.py
@@ -1,21 +1,23 @@
 import curses
+import requests
 from curses import wrapper
 
 
+menu = ["Play Typing Game", "Tell Me A Dad Joke", "Exit"] 
+
 def print_menu(stdscr, selected):
-    menu = ["Start Game","Tell me a Dad Joke!","Exit"]
     stdscr.clear()
     height, width = stdscr.getmaxyx()
     for idx, row in enumerate(menu):
         x = width // 2 - len(row) // 2
-        y = height // 2 - len(row) // 2
+        y = height // 2 - len(menu) // 2 + idx
         if idx == selected:
             stdscr.attron(curses.A_REVERSE)
             stdscr.addstr(y, x , row)
             stdscr.attroff(curses.A_REVERSE)
         else:
             stdscr.addstr(y, x, row)
-        stdscr.noutrefresh()
+    stdscr.noutrefresh()
         
         
 def make_me_laugh(stdscr):
@@ -26,14 +28,10 @@ def typing_game(stdscr):
     pass
     
     
-def update_home_screen():
-    pass
-        
         
 def main(stdscr):
     
     def home_screen(stdscr):
-        height, width = stdscr.getmaxyx()
         
         curses.curs_set(0)
         current_row = 0
@@ -49,12 +47,13 @@ def main(stdscr):
                 current_row += 1
             elif key == curses.KEY_ENTER or key in [10,13]:
                 if current_row == 0:
-                    typing_game()
+                    typing_game(stdscr)
                 if current_row == 1:
                     make_me_laugh(stdscr)
                 if current_row == 2:
                     break
-        print_menu(stdscr, current_row)
+            print_menu(stdscr, current_row)
+            curses.doupdate()
 
                     
     
