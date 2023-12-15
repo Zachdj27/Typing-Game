a3c2905f79e8826b9e1e4ea0309d0a218e7e234f
Zachary Dj <zachary@zacharys-mbp.home>
2023-12-14 20:59:51 -0500

    finalized game

--- main_game.py
+++ main_game.py
@@ -25,26 +25,28 @@ def print_menu(stdscr, selected):
         
         
 def make_me_laugh(stdscr):
-    stdscr.clear()
-    while True:
-        #get rid of exceptions from huge texts
-        try:
-            setup = joke = fetch_joke()
-            punchline = ""
-            if "?" in joke:
-                setup, punchline = [str(line) for line in joke.split("?")]
-            
-            height, width = stdscr.getmaxyx()
-            x_setup = width // 2 - len(setup) // 2
-            x_punchline = width // 2 - len(punchline) // 2
-            y = height // 2
-            stdscr.addstr(y, x_setup, setup + '?' * ('?' in joke))
-            stdscr.addstr(y + 1, x_punchline, punchline)
-            break
-        except curses.error:
+   stdscr.clear()
+   while True:
+       # i’m using try/except to get rid of exceptions from huge texts
+       try:
+           setup = joke = fetch_joke()
+           punchline = ''
+           if '?' in joke:
+               setup, punchline = [str(line) for line in joke.split('?')]
+
+
+           height, width = stdscr.getmaxyx()
+           x_setup = width // 2 - len(setup) // 2
+           x_punchline = width // 2 - len(punchline) // 2
+           y = height // 2
+           stdscr.addstr(y, x_setup, setup + '?' * ('?' in joke))
+           stdscr.addstr(y + 1, x_punchline, punchline)
+           break
+       except curses.error:
            continue
-    stdscr.refresh()
-    stdscr.getch()
+   stdscr.refresh()
+   stdscr.getch()
+
            
 def home_screen(stdscr):
         
@@ -58,21 +60,34 @@ def home_screen(stdscr):
         key = stdscr.getch()
         if key == curses.KEY_UP and current_row > 0:
             current_row -= 1
-        elif key == curses.KEY_DOWN and current_row < 2:
+        elif key == curses.KEY_DOWN and current_row < 1:
             current_row += 1
         elif key == curses.KEY_ENTER or key in [10,13]:
             if current_row == 0:
                 wpm_test(stdscr)
+                while(True):
+                    stdscr.addstr(2, 0, "Congrats on completing the text! Press any key to conitnue or press escape to quit." )
+                    key = stdscr.getkey()
+                    
+                    if ord(key) == 27:
+                        break
+                    else:
+                        wpm_test(stdscr)
+                        
             if current_row == 1:
                 make_me_laugh(stdscr)
             if current_row == 2:
                 break
+            
         print_menu(stdscr, current_row)
         curses.doupdate()    
 
 def display_text(stdscr, target, current, wpm = 0):
-        stdscr.addstr(target)
-        stdscr.addstr(1, 0, f"WPM: {wpm}")
+        height, width = stdscr.getmaxyx()
+        x = width // 2 - len(target) // 2
+        y = height // 2
+        stdscr.addstr(y , x, target)
+        stdscr.addstr(y + 2, x, f"WPM: {wpm}")
         
         for i, char in enumerate(current):
             correct_char = target[i]
@@ -80,24 +95,27 @@ def display_text(stdscr, target, current, wpm = 0):
             if char != correct_char:
                 color = curses.color_pair(2)
           
-            stdscr.addstr(0, i, char, color)
+            stdscr.addstr(y, i + x, char, color)
 
 
 def wpm_test(stdscr):
-    target_text = "Hello world. This is some test text for this app!"
     current_text = []
     wpm = 0
     start_time = time.time()
     stdscr.nodelay(True)
+    target = fetch_joke()
     
     while True:
         time_elapsed = max(time.time() - start_time, 1)
         wpm = round((len(current_text) / (time_elapsed/60)) / 5)
         
         stdscr.clear()
-        display_text(stdscr, target_text, current_text, wpm)
+        display_text(stdscr, target, current_text, wpm)
         stdscr.refresh()
-      
+
+        if "".join(current_text) == target:
+            stdscr.nodelay(False)
+            break
         try:
             key = stdscr.getkey()
         except:
@@ -108,7 +126,7 @@ def wpm_test(stdscr):
         if key in ("KEY_BACKSPACE", '\b', '\x7f'):
             if len(current_text) > 0:
                 current_text.pop()
-        elif len(current_text) < len(target_text):
+        elif len(current_text) < len(target):
             current_text.append(key)
         
         
