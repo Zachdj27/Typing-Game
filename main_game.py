c5ea1ac6a6f12002c37865f793f9087a855f1525
Zachary Dj <zachary@zacharys-mbp.home>
2023-12-14 19:09:23 -0500

    added typing game mode

--- main_game.py
+++ main_game.py
@@ -5,6 +5,9 @@ from curses import wrapper
 
 menu = ["Play Typing Game", "Tell Me A Dad Joke", "Exit"] 
 
+def fetch_joke():
+    return requests.get("https://icanhazdadjoke.com/", headers={"Accept": "text/plain"}).content.decode('U8')
+
 def print_menu(stdscr, selected):
     stdscr.clear()
     height, width = stdscr.getmaxyx()
@@ -21,43 +24,71 @@ def print_menu(stdscr, selected):
         
         
 def make_me_laugh(stdscr):
-    pass
-                
-                
-def typing_game(stdscr):
-    pass
+    stdscr.clear()
+    while True:
+        #get rid of exceptions from huge texts
+        try:
+            setup = joke = fetch_joke()
+            punchline = ""
+            if "?" in joke:
+                setup, punchline = [str(line) for line in joke.split("?")]
+            
+            height, width = stdscr.getmaxyx()
+            x_setup = width // 2 - len(setup) // 2
+            x_punchline = width // 2 - len(punchline) // 2
+            y = height // 2
+            stdscr.addstr(y, x_setup, setup + '?' * ('?' in joke))
+            stdscr.addstr(y + 1, x_punchline, punchline)
+            break
+        except curses.error:
+           continue
+    stdscr.refresh()
+    stdscr.getch()
+           
+def home_screen(stdscr):
+        
+    curses.curs_set(0)
+    current_row = 0
+    print_menu(stdscr, current_row)
+        
+    curses.doupdate()
+        
+    while True:
+        key = stdscr.getch()
+        if key == curses.KEY_UP and current_row > 0:
+            current_row -= 1
+        elif key == curses.KEY_DOWN and current_row < 2:
+            current_row += 1
+        elif key == curses.KEY_ENTER or key in [10,13]:
+            if current_row == 0:
+                wpm_test(stdscr)
+            if current_row == 1:
+                make_me_laugh(stdscr)
+            if current_row == 2:
+                break
+        print_menu(stdscr, current_row)
+        curses.doupdate()    
     
+def wpm_test(stdscr):
+    target_text = "Hello world. This is some test text for this app!"
+    current_text = []
     
+    while True:
+        key = stdscr.getkey()
+        current_text.append(key)
+        
+        stdscr.clear()
+        stdscr.addstr(target_text)
+        
+        for char in current_text:
+            stdscr.addstr(char, curses.color_pair(1) )
+        stdscr.refresh()
+      
         
 def main(stdscr):
-    
-    def home_screen(stdscr):
-        
-        curses.curs_set(0)
-        current_row = 0
-        print_menu(stdscr, current_row)
-        
-        curses.doupdate()
-        
-        while True:
-            key = stdscr.getch()
-            if key == curses.KEY_UP and current_row > 0:
-                current_row -= 1
-            elif key == curses.KEY_DOWN and current_row < 2:
-                current_row += 1
-            elif key == curses.KEY_ENTER or key in [10,13]:
-                if current_row == 0:
-                    typing_game(stdscr)
-                if current_row == 1:
-                    make_me_laugh(stdscr)
-                if current_row == 2:
-                    break
-            print_menu(stdscr, current_row)
-            curses.doupdate()
-
-                    
-    
-    home_screen(stdscr)
+  curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_WHITE)
+      
+  home_screen(stdscr)
 
     
 wrapper(main)
